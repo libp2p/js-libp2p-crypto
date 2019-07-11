b85245638f57e398599a78bf8f7b8fbe6157a3cb
Dirk McCormick <dirkmdev@gmail.com>
2019-07-11 10:15:50 -0400

    refactor: simplify random bytes error checks

--- src/random-bytes.js
+++ src/random-bytes.js
@@ -3,7 +3,7 @@ const randomBytes = require('iso-random-stream/src/random')
 const errcode = require('err-code')
 
 module.exports = function (length) {
-  if (!length || typeof length !== 'number' || length <= 0) {
+  if (isNaN(length) || length <= 0) {
     throw errcode(new Error('random bytes length must be a Number bigger than 0'), 'ERR_INVALID_LENGTH')
   }
   return randomBytes(length)

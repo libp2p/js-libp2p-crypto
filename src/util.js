21b7279e84cf8d756ade1f492d149bc50722166f
Alan Shaw <alan.shaw@protocol.ai>
2020-02-14 15:35:33 +0000

    perf: remove asn1.js from rsa

--- src/util.js
+++ src/util.js
@@ -1,6 +1,10 @@
 'use strict'
 
 const BN = require('asn1.js').bignum
+const { Buffer } = require('buffer')
+require('node-forge/lib/util')
+require('node-forge/lib/jsbn')
+const forge = require('node-forge/lib/forge')
 
 // Convert a BN.js instance to a base64 encoded string without padding
 // Adapted from https://tools.ietf.org/html/draft-ietf-jose-json-web-signature-41#appendix-C
@@ -12,6 +16,21 @@ exports.toBase64 = function toBase64 (bn, len) {
     .replace(/(=*)$/, '') // Remove any trailing '='s
     .replace(/\+/g, '-') // 62nd char of encoding
     .replace(/\//g, '_') // 63rd char of encoding
+}
+
+exports.bufferToBase64url = buf => {
+  return buf.toString('base64')
+    .split('=')[0] // Remove any trailing '='s
+    .replace(/\+/g, '-') // 62nd char of encoding
+    .replace(/\//g, '_') // 63rd char of encoding
+}
+
+exports.base64urlToBigInteger = str => {
+  str = (str + '==='.slice((str.length + 3) % 4))
+    .replace(/-/g, '+')
+    .replace(/_/g, '/')
+  const bytes = forge.util.decode64(str)
+  return new forge.jsbn.BigInteger(forge.util.bytesToHex(bytes), 16)
 }
 
 // Convert a base64 encoded string to a BN.js instance

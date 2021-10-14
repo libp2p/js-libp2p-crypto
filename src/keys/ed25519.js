9e26a6add5d0349bd78628854b7c7b99011d348d
Hugo Dias <hugomrdias@gmail.com>
2021-10-14 16:16:56 +0100

    fix: use noble-ed25519

--- src/keys/ed25519.js
+++ src/keys/ed25519.js
@@ -1,12 +1,19 @@
 'use strict'
 
-require('node-forge/lib/ed25519')
-const forge = require('node-forge/lib/forge')
-exports.publicKeyLength = forge.pki.ed25519.constants.PUBLIC_KEY_BYTE_LENGTH
-exports.privateKeyLength = forge.pki.ed25519.constants.PRIVATE_KEY_BYTE_LENGTH
+// require('node-forge/lib/ed25519')
+// const forge = require('node-forge/lib/forge')
+const ed = require('noble-ed25519')
 
-exports.generateKey = async function () { // eslint-disable-line require-await
-  return forge.pki.ed25519.generateKeyPair()
+exports.publicKeyLength = 32 //forge.pki.ed25519.constants.PUBLIC_KEY_BYTE_LENGTH
+exports.privateKeyLength = 64 // forge.pki.ed25519.constants.PRIVATE_KEY_BYTE_LENGTH
+
+exports.generateKey = async function () {
+  // const pair= forge.pki.ed25519.generateKeyPair()
+  const privateKey = ed.utils.randomPrivateKey()
+  return {
+    privateKey,
+    publicKey: await ed.getPublicKey(privateKey)
+  }
 }
 
 // seed should be a 32 byte uint8array
@@ -14,11 +21,15 @@ exports.generateKeyFromSeed = async function (seed) { // eslint-disable-line req
   return forge.pki.ed25519.generateKeyPair({ seed })
 }
 
-exports.hashAndSign = async function (key, msg) { // eslint-disable-line require-await
-  return forge.pki.ed25519.sign({ message: msg, privateKey: key })
+exports.hashAndSign = function (key, msg) { // eslint-disable-line require-await
+  // return forge.pki.ed25519.sign({ message: msg, privateKey: key })
   // return Uint8Array.from(nacl.sign.detached(msg, key))
+
+  return ed.sign(msg, key)
 }
 
 exports.hashAndVerify = async function (key, sig, msg) { // eslint-disable-line require-await
-  return forge.pki.ed25519.verify({ signature: sig, message: msg, publicKey: key })
+  // return forge.pki.ed25519.verify({ signature: sig, message: msg, publicKey: key })
+
+  return ed.verify(sig, msg, key)
 }

562d5e10c73c7fa1d8f99de6439e151cb6d0b30e
Hugo Dias <hugomrdias@gmail.com>
2021-10-21 10:24:18 +0000

    fix: feedback

--- src/keys/ed25519.js
+++ src/keys/ed25519.js
@@ -2,8 +2,12 @@
 
 const ed = require('noble-ed25519')
 
-exports.publicKeyLength = 32
-exports.privateKeyLength = 64 // private key is 32 bytes actual private key and 32 bytes public key, for historical reasons
+const PUBLIC_KEY_BYTE_LENGTH = 32
+const PRIVATE_KEY_BYTE_LENGTH = 64 // private key is actually 32 bytes but for historical reasons we concat private and public keys
+const KEYS_BYTE_LENGTH = 32
+
+exports.publicKeyLength = PUBLIC_KEY_BYTE_LENGTH
+exports.privateKeyLength = PRIVATE_KEY_BYTE_LENGTH
 
 exports.generateKey = async function () {
   // the actual private key (32 bytes)
@@ -19,15 +23,20 @@ exports.generateKey = async function () {
   }
 }
 
-// seed should be a 32 byte uint8array
-exports.generateKeyFromSeed = async function (seed) { // eslint-disable-line require-await
-  if (seed.length !== 32) {
+/**
+ * Generate keypair from a seed
+ *
+ * @param {Uint8Array} seed - seed should be a 32 byte uint8array
+ * @returns
+ */
+exports.generateKeyFromSeed = async function (seed) {
+  if (seed.length !== KEYS_BYTE_LENGTH) {
     throw new TypeError('"seed" must be 32 bytes in length.')
   } else if (!(seed instanceof Uint8Array)) {
     throw new TypeError('"seed" must be a node.js Buffer, or Uint8Array.')
   }
 
-  // based on node.forges algorithm, the seed is used directly as private key
+  // based on node forges algorithm, the seed is used directly as private key
   const privateKeyRaw = seed
   const publicKey = await ed.getPublicKey(privateKeyRaw)
 
@@ -39,21 +48,21 @@ exports.generateKeyFromSeed = async function (seed) { // eslint-disable-line req
   }
 }
 
-exports.hashAndSign = function (privateKey, msg) { // eslint-disable-line require-await
-  const privateKeyRaw = privateKey.slice(0, 32)
+exports.hashAndSign = function (privateKey, msg) {
+  const privateKeyRaw = privateKey.slice(0, KEYS_BYTE_LENGTH)
 
   return ed.sign(msg, privateKeyRaw)
 }
 
-exports.hashAndVerify = async function (publicKey, sig, msg) { // eslint-disable-line require-await
+exports.hashAndVerify = function (publicKey, sig, msg) {
   return ed.verify(sig, msg, publicKey)
 }
 
 function concatKeys (privateKeyRaw, publicKey) {
   const privateKey = new Uint8Array(exports.privateKeyLength)
-  for (let i = 0; i < 32; i++) {
+  for (let i = 0; i < KEYS_BYTE_LENGTH; i++) {
     privateKey[i] = privateKeyRaw[i]
-    privateKey[32 + i] = publicKey[i]
+    privateKey[KEYS_BYTE_LENGTH + i] = publicKey[i]
   }
   return privateKey
 }

df7c682a0810328c64593ed614a6c2177ee40064
achingbrain <alex@achingbrain.net>
2022-04-09 15:04:18 +0100

    chore: fix browser overrides

--- .aegir.js
+++ .aegir.js
@@ -1,17 +1,7 @@
 
 /** @type {import('aegir/types').PartialOptions} */
 export default {
-  test: {
-    browser: {
-      config: {
-        entryPoints: ['./dist/src/index.js']
-      }
-    }
-  },
   build: {
-    bundlesizeMax: '66kB',
-    config: {
-      entryPoints: ['./dist/src/index.js']
-    }
+    bundlesizeMax: '66kB'
   }
 }
